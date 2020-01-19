7b95c33a36779cd6fc1e1116fefee82de4f6fe44
Caio <ca.ragazzi@gmail.com>
2020-01-19 13:52:28 -0300

    first commit

---
+++ WebApi/Controllers/WeatherForecastController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        IWeatherForecast _weather;
+
+        public WeatherForecastController(IWeatherForecast weather)
+        {
+            _weather = weather;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            List<WeatherForecast> weathers = new List<WeatherForecast>
+            {
+                new WeatherForecast(),
+                new WeatherForecast(),
+                new WeatherForecast(),
+            };
+
+            return weathers;
+        }
+    }
+}
---
+++ WebApi/Models/IWeatherForecast.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WebApi
+{
+    public interface IWeatherForecast
+    {
+        DateTime Date { get; set; }
+        string Summary { get; set; }
+        int TemperatureC { get; set; }
+        int TemperatureF { get; }
+    }
+}
